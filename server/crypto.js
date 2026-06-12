63a7545193ac899147d28deb2ffe3b2294608fd7
鹿全龙 <luql@yeyoo.cn>
2026-06-12 08:39:53 +0800

    导入导出 bug修复

--- server/crypto.js
+++ server/crypto.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const KEY_FILE = path.join(__dirname, '.crypto_key');
 const ALGORITHM = 'aes-256-gcm';
-const SENSITIVE_KEYS = ['zhipu_api_key', 'zhipu_api_keys', 'ai_configs', 'ai_vendor_configs', 'ai_dispatch_order', 'imgbb_api_key', 'comfyui_creds'];
+const SENSITIVE_KEYS = ['zhipu_api_key', 'zhipu_api_keys', 'ai_configs', 'ai_vendor_configs', 'ai_dispatch_order', 'imgbb_api_key', 'comfyui_creds', 'oss_access_key_id', 'oss_access_key_secret', 'oss_bucket', 'oss_region', 'oss_endpoint', 'turso_config'];
 
 var _key = null;
 
