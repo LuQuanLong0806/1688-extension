831712a62a401f916ccd206e4e0dcea486ce292f
鹿全龙 <luql@yeyoo.cn>
2026-06-22 09:01:07 +0800

    配置增加

---
+++ sites/build-info.js
@@ -0,0 +1,3 @@
+// 自动生成，请勿手动修改。打包时间：2026-06-16 16:14:54 (UTC+8)
+window.__EXT_BUILD_TIME__ = "2026-06-16 16:14:54";
+console.log("%c[扩展] 打包时间: 2026-06-16 16:14:54 (UTC+8)", "color:#E65100;font-weight:bold;font-size:13px;");
